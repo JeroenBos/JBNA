aa4ec3b7e45c487be6dae3eb3552ff906c8f81ae
Jeroen Bos <jer123bos@gmail.com>
2022-02-20 00:00:27 +0100

    Add BitReader Tests + bugfix

--- JBNA/BitReader.cs
+++ JBNA/BitReader.cs
@@ -1,16 +1,36 @@
-class BitReader
+using BitArray = JBSnorro.Collections.BitArray;
+
+public class BitReader
 {
-    private readonly JBSnorro.Collections.BitArray data;
+    private readonly BitArray data;
     private readonly ulong[] _data;
+    /// <summary> In bits. </summary>
+    public int Length => this.data.Length;
     public bool this[int longIndex, int bitindex] => this.data[longIndex * 64 + bitIndex];
 
+    /// <summary> In bits. </summary>
     private int current;
-    private int ulongIndex => current % 64;
+    private int ulongIndex => current / 64;
     private int bitIndex => current % 64;
-    public int RemainingBitCount => this.data.Length - current;
-    public BitReader(JBSnorro.Collections.BitArray data, int startBitIndex = 0)
+    /// <summary> In bits. </summary>
+    public int RemainingLength => this.Length - current;
+    public BitReader(BitArray data, int startBitIndex = 0)
     {
         this.data = data;
+        this.current = startBitIndex;
+
+        // HACK until next version of library 
+        this._data = (ulong[])typeof(BitArray).GetField("data", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)!.GetValue(this.data)!;
+    }
+    public BitReader(ulong[] data, int length, int startBitIndex = 0)
+    {
+        this._data = data;
+        this.data = new JBSnorro.Collections.BitArray();
+        // HACK until next version of library 
+        typeof(BitArray).GetField("data", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)!.SetValue(this.data, this._data);
+        typeof(BitArray).GetField(_getBackingFieldName("Length"), System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic)!.SetValue(this.data, length);
+        string _getBackingFieldName(string propertyName) => string.Format("<{0}>k__BackingField", propertyName);
+
         this.current = startBitIndex;
     }
 
@@ -20,7 +40,7 @@ class BitReader
     }
     public bool ReadBit()
     {
-        if (this.RemainingBitCount < 1)
+        if (this.RemainingLength < 1)
             throw InsufficientBitsException("bit");
 
         bool result = this[ulongIndex, bitIndex];
@@ -31,7 +51,7 @@ class BitReader
     {
         if (bitCount < 1 || bitCount > 8)
             throw new ArgumentException(nameof(bitCount));
-        if (this.RemainingBitCount < bitCount)
+        if (this.RemainingLength < bitCount)
             throw InsufficientBitsException("byte");
 
         return (byte)_Read(bitCount);
@@ -40,17 +60,17 @@ class BitReader
     {
         if (bitCount < 1 || bitCount > 16)
             throw new ArgumentException(nameof(bitCount));
-        if (this.RemainingBitCount < bitCount)
+        if (this.RemainingLength < bitCount)
             throw InsufficientBitsException("short");
 
         return (short)_Read(bitCount);
     }
-    
+
     public int ReadInt32(int bitCount = 32)
     {
         if (bitCount < 1 || bitCount > 32)
             throw new ArgumentException(nameof(bitCount));
-        if (this.RemainingBitCount < bitCount)
+        if (this.RemainingLength < bitCount)
             throw InsufficientBitsException("int");
 
         return (int)_Read(bitCount);
@@ -59,14 +79,14 @@ class BitReader
     {
         if (bitCount < 1 || bitCount > 64)
             throw new ArgumentException(nameof(bitCount));
-        if (this.RemainingBitCount < bitCount)
+        if (this.RemainingLength < bitCount)
             throw InsufficientBitsException("long");
 
         return (long)_Read(bitCount);
     }
     public float ReadSingle()
     {
-        if (this.RemainingBitCount < 32)
+        if (this.RemainingLength < 32)
             throw InsufficientBitsException("Single");
         int i = ReadInt32();
         unsafe
@@ -79,7 +99,7 @@ class BitReader
     }
     public Half ReadHalf()
     {
-        if (this.RemainingBitCount < 16)
+        if (this.RemainingLength < 16)
             throw InsufficientBitsException("Half");
         short i = ReadInt16();
         unsafe
@@ -97,7 +117,7 @@ class BitReader
             throw new ArgumentException(nameof(bitCount));
 
         ulong result;
-        if (bitIndex + bitCount < 64)
+        if (bitIndex + bitCount <= 64)
         {
             ulong shifted = this._data[ulongIndex] >> bitIndex;
             ulong mask = ulong.MaxValue >> (64 - bitCount);
@@ -107,7 +127,7 @@ class BitReader
         {
             ulong shiftedP1 = this._data[ulongIndex] >> this.bitIndex;
             ulong maskP1 = ulong.MaxValue >> (64 - this.bitIndex);
-            ulong shiftedP2 = this._data[ulongIndex + 1] << (bitCount - this.bitIndex);
+            ulong shiftedP2 = this._data[ulongIndex + 1] << ((this.bitIndex + bitCount) % 64);
             ulong maskP2 = ulong.MaxValue >> (64 - bitCount);
 
             result = (shiftedP1 & maskP1) | (shiftedP2 & maskP2);
@@ -116,5 +136,12 @@ class BitReader
         current += bitCount;
 
         return result;
+    }
+
+    public void Seek(int bitIndex)
+    {
+        if (bitIndex > this.Length)
+            throw new ArgumentOutOfRangeException(nameof(bitIndex));
+        this.current = bitIndex;
     }
 }
---
+++ JBNA/Folders/BinaryReaderTests.cs
@@ -0,0 +1,92 @@
+using Xunit;
+
+public class BinaryReaderTests
+{
+    [Fact]
+    public void Can_Construct()
+    {
+        var reader = new BitReader(new JBSnorro.Collections.BitArray());
+        Assert(reader.Length == 0);
+    }
+    [Fact]
+    public void Can_Read_FalseBit()
+    {
+        var reader = new BitReader(new JBSnorro.Collections.BitArray(new bool[] { false }));
+        Assert(reader.ReadBit() == false);
+    }
+    [Fact]
+    public void Can_Read_TrueBit()
+    {
+        var reader = new BitReader(new JBSnorro.Collections.BitArray(new bool[] { true }));
+        Assert(reader.ReadBit() == true);
+    }
+    [Fact]
+    public void Can_Read_Zero_Byte()
+    {
+        var reader = new BitReader(new[] { 0b0UL }, 8);
+        Assert(reader.ReadByte() == 0);
+    }
+
+    [Fact]
+    public void Can_Read_One_Byte()
+    {
+        var reader = new BitReader(new[] { 0b0000_0001UL }, 8);
+        Assert(reader.ReadByte() == 1);
+    }
+    [Fact]
+    public void Can_Read_Two_Byte()
+    {
+        var reader = new BitReader(new[] { 0b0000_0010UL }, 8);
+        Assert(reader.ReadByte() == 2);
+    }
+
+    [Fact]
+    public void Can_Read_Two_ULong()
+    {
+        var reader = new BitReader(new[] { 0b0000_0010UL }, 64);
+        Assert(reader.ReadInt64() == 2);
+    }
+    [Fact]
+    public void Reading_bits_is_successive()
+    {
+        var reader = new BitReader(new[] { 0b0000_0110UL }, 8);
+        Assert(reader.ReadBit() == false);
+        Assert(reader.ReadBit() == true);
+        Assert(reader.ReadBit() == true);
+        Assert(reader.ReadBit() == false);
+        Assert(reader.ReadBit() == false);
+        Assert(reader.RemainingLength == 3);
+    }
+    [Fact]
+    public void Reading_bytes_is_successive()
+    {
+        var reader = new BitReader(new[] { 0b1111_0000_0000_0110UL }, 16);
+        Assert(reader.ReadByte() == 0b110);
+        Assert(reader.RemainingLength == 8);
+        Assert(reader.ReadByte() == 0b1111_0000);
+        Assert(reader.RemainingLength == 0);
+    }
+    [Fact]
+    public void Reading_bits_and_bytes_is_successive()
+    {
+        var reader = new BitReader(new[] { 0b1101_0111_0100_0000_0110UL }, 20);
+        Assert(reader.ReadBit() == false);
+        Assert(reader.RemainingLength == 19);
+        Assert(reader.ReadByte() == 0b0000_0011);
+        Assert(reader.RemainingLength == 11);
+        Assert(reader.ReadBit() == false);
+        Assert(reader.ReadBit() == true);
+        Assert(reader.ReadBit() == false);
+        Assert(reader.RemainingLength == 8);
+        Assert(reader.ReadByte() == 0b1101_0111);
+    }
+    [Fact]
+    public void Can_read_bytes_over_ulong_crossing()
+    {
+        var reader = new BitReader(new[] { (0b1001UL << 60) | 1234, 0b1100UL }, 100);
+        var x = reader.ReadInt64(bitCount: 60);
+        Assert(x == 1234);
+        var y = reader.ReadByte();
+        Assert(y == 0b1100_1001);
+    }
+}
