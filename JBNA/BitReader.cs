f13b57f6f7a34703e3a8a68db70f346e92d6fc3a
Jeroen Bos <jer123bos@gmail.com>
2022-03-20 18:10:14 +0100

    Improve BitReader

--- JBNA/BitReader.cs
+++ JBNA/BitReader.cs
@@ -1,9 +1,26 @@
 using BitArray = JBSnorro.Collections.BitArray;
 
-public class BitReader
+namespace JBSnorro;
+
+public interface IBitReader
 {
-    private readonly BitArray data;
+    int Length { get; }
+    bool ReadBit();
+    byte ReadByte(int bitCount);
+    short ReadInt16(int bitCount);
+    int ReadInt32(int bitCount);
+    long ReadInt64(int bitCount);
+    ulong ReadUInt64(int bitCount);
+
+    Half ReadHalf();
+    float ReadSingle();
+    double ReadDouble();
+    void Seek(int bitIndex);
+}
+public class BitReader : IBitReader
+{
     private readonly ulong[] _data;
+    private readonly BitArray data;
     /// <summary> In bits. </summary>
     public int Length => this.data.Length;
     public bool this[int longIndex, int bitindex] => this.data[longIndex * 64 + bitIndex];
@@ -54,7 +71,7 @@ public class BitReader
         if (this.RemainingLength < bitCount)
             throw InsufficientBitsException("byte");
 
-        return (byte)_Read(bitCount);
+        return (byte)ReadUInt64(bitCount);
     }
     public short ReadInt16(int bitCount = 16)
     {
@@ -63,7 +80,7 @@ public class BitReader
         if (this.RemainingLength < bitCount)
             throw InsufficientBitsException("short");
 
-        return (short)_Read(bitCount);
+        return (short)ReadUInt64(bitCount);
     }
 
     public int ReadInt32(int bitCount = 32)
@@ -73,7 +90,7 @@ public class BitReader
         if (this.RemainingLength < bitCount)
             throw InsufficientBitsException("int");
 
-        return (int)_Read(bitCount);
+        return (int)ReadUInt64(bitCount);
     }
     public long ReadInt64(int bitCount = 64)
     {
@@ -82,20 +99,7 @@ public class BitReader
         if (this.RemainingLength < bitCount)
             throw InsufficientBitsException("long");
 
-        return (long)_Read(bitCount);
-    }
-    public float ReadSingle()
-    {
-        if (this.RemainingLength < 32)
-            throw InsufficientBitsException("Single");
-        int i = ReadInt32();
-        unsafe
-        {
-            int* pointer = &i;
-            float* floatPointer = (float*)pointer;
-            float result = *floatPointer;
-            return result;
-        }
+        return (long)ReadUInt64(bitCount);
     }
     public Half ReadHalf()
     {
@@ -110,8 +114,35 @@ public class BitReader
             return result;
         }
     }
+    public float ReadSingle()
+    {
+        if (this.RemainingLength < 32)
+            throw InsufficientBitsException("Single");
+        int i = ReadInt32();
+        unsafe
+        {
+            int* pointer = &i;
+            float* floatPointer = (float*)pointer;
+            float result = *floatPointer;
+            return result;
+        }
+    }
+    public double ReadDouble()
+    {
+        if (this.RemainingLength < 64)
+            throw InsufficientBitsException("Double");
+        long i = ReadInt64();
+        unsafe
+        {
+            long* pointer = &i;
+            double* doublePointer = (double*)pointer;
+            double result = *doublePointer;
+            return result;
+        }
+    }
 
-    private ulong _Read(int bitCount)
+
+    public ulong ReadUInt64(int bitCount)
     {
         if (bitCount < 1 || bitCount > 64)
             throw new ArgumentException(nameof(bitCount));
@@ -120,15 +151,15 @@ public class BitReader
         if (bitIndex + bitCount <= 64)
         {
             ulong shifted = this._data[ulongIndex] >> bitIndex;
-            ulong mask = ulong.MaxValue >> (64 - bitCount);
+            ulong mask = LowerBitsMask(bitCount);
             result = shifted & mask;
         }
         else
         {
             ulong shiftedP1 = this._data[ulongIndex] >> this.bitIndex;
-            ulong maskP1 = ulong.MaxValue >> (64 - this.bitIndex);
+            ulong maskP1 = LowerBitsMask(this.bitIndex);
             ulong shiftedP2 = this._data[ulongIndex + 1] << ((this.bitIndex + bitCount) % 64);
-            ulong maskP2 = ulong.MaxValue >> (64 - bitCount);
+            ulong maskP2 = LowerBitsMask(bitCount);
 
             result = (shiftedP1 & maskP1) | (shiftedP2 & maskP2);
         }
@@ -140,8 +171,83 @@ public class BitReader
 
     public void Seek(int bitIndex)
     {
-        if (bitIndex > this.Length)
+        if (bitIndex < 0 || bitIndex > this.Length)
             throw new ArgumentOutOfRangeException(nameof(bitIndex));
         this.current = bitIndex;
+    }
+    /// <summary>
+    /// Gets the index in the stream the pattern occurs at.
+    /// </summary>
+    public int Find(ulong pattern, int patternLength)
+    {
+        if (patternLength < 1)
+            throw new ArgumentOutOfRangeException(nameof(patternLength));
+        if (patternLength > 64)
+            throw new NotSupportedException("patternLength > 64");
+
+        ulong mask = LowerBitsMask(patternLength);
+
+        for (; this.current + patternLength < this.Length; this.current -= (patternLength - 1))
+        {
+            ulong current = this.ReadUInt64(patternLength);
+            if (((current ^ pattern) & mask) == mask)
+            {
+                return this.current - patternLength;
+            }
+        }
+        this.current = this.Length;
+        return -1;
+    }
+    /// <summary>
+    /// Gets the index in the stream the pattern occurs at.
+    /// </summary>
+    public int Find(ulong pattern, int patternLength, int startBitIndex)
+    {
+        if (startBitIndex < 0 || startBitIndex > this.Length)
+            throw new ArgumentOutOfRangeException(nameof(startBitIndex));
+        if (patternLength < 1)
+            throw new ArgumentOutOfRangeException(nameof(patternLength));
+        if (patternLength > 64)
+            throw new ArgumentOutOfRangeException(nameof(patternLength), "> 64");
+
+        this.Seek(startBitIndex);
+        return this.Find(pattern, patternLength, startBitIndex);
+    }
+
+    /// <param name="length"> In bits. </param>
+    public void CopyTo(ulong[] dest, int startBitIndex, int length, int destBitIndex)
+    {
+        if (dest is null)
+            throw new ArgumentNullException(nameof(dest));
+        if (length < 0)
+            throw new ArgumentOutOfRangeException(nameof(length));
+        if (startBitIndex < 0 || startBitIndex + length > this.Length)
+            throw new ArgumentOutOfRangeException(nameof(length));
+        if (destBitIndex < 0 || destBitIndex + length > dest.Length * 64)
+            throw new ArgumentOutOfRangeException(nameof(destBitIndex));
+        
+        this.data.CopyTo(dest, destBitIndex);
+
+    }
+
+    /// <summary> Returns <see param="bitCount"/> ones followed by zeroes (least significant to most). </summary>
+    private static ulong LowerBitsMask(int bitCount)
+    {
+        return ulong.MaxValue >> (64 - bitCount);
+    }
+    /// <summary> Returns zeroes ending on <see param="bitCount"/> ones (least significant to most). </summary>
+    private static ulong UpperBitsMask(int bitCount)
+    {
+        return ~UpperBitsUnmask(bitCount);
+    }
+    /// <summary> Returns <see param="bitCount"/> zeroes followed by ones (least significant to most). </summary>
+    private static ulong LowerBitsUnmask(int bitCount)
+    {
+        return ~LowerBitsMask(bitCount);
+    }
+    /// <summary> Returns ones ending on <see param="bitCount"/> zeroes (least significant to most). </summary>
+    private static ulong UpperBitsUnmask(int bitCount)
+    {
+        return ulong.MaxValue << bitCount;
     }
 }
